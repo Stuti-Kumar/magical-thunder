533082bdacd3c00f9180437701cca5d77b6c9137
Stuti Kumar <kumarstuti@johndeere.com>
2022-12-21 14:28:48 +0530

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# magical_thunder
+This repo is to test and create the artifacts to replace Jenkins with GHA  
---
+++ fancyTest.m
@@ -0,0 +1,9 @@
+function tests = fancyTest
+tests = functiontests(localfunctions);
+end
+
+
+function testFanciness(testCase)
+s = getFancy;
+testCase.assertEqual(s,'pretty fancy');
+end
