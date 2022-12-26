f8d19f09dd2ab6ba6417d76441782d69695c344f
Stuti Kumar <kumarstuti@johndeere.com>
2022-12-26 18:57:49 +0530

    Verify working on 2 different MATLAB version

--- fancyTest.m
+++ fancyTest.m
@@ -7,3 +7,15 @@ function testFanciness(testCase)
 s = getFancy;
 testCase.assertEqual(s,'pretty fancy');
 end
+
+function testMatlabVer1(testCase)
+testCase.assumeTrue( verLessThan('matlab','9.10'),'Test version 20b');
+releaseInfo = matlabRelease;
+testCase.assertEqual(releaseInfo.Release,"R2020b");
+end
+
+function testMatlabVer2(testCase)
+testCase.assumeTrue( ~verLessThan('matlab','9.13'),'Test Case supported for version R2022b onwards');
+releaseInfo = matlabRelease;
+testCase.assertEqual(releaseInfo.Release,"R2022b");
+end
