30c962d1de119a8df85f27e9d1bf4cf6060af658
Stuti Kumar <kumarstuti@johndeere.com>
2022-12-28 16:56:15 +0530

    displaying messages

--- RunMyTests.m
+++ RunMyTests.m
@@ -1,5 +1,8 @@
 restoredefaultpath
+disp('Step 1')
 cd('UnitTest'); 
-addpath('Tests');
+% addpath('Tests');
+disp('Step 2')
 prj = simulinkproject('DemoToolTest.prj');
+disp('Step 3')
 RunToolTests
