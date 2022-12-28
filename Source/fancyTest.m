c036a7f684f7e962e0d4bef700b27e207803d5e6
Stuti Kumar <kumarstuti@johndeere.com>
2022-12-28 17:12:56 +0530

    adding test file in source folder

---
+++ Source/fancyTest.m
@@ -0,0 +1,76 @@
+classdef fancyTest < matlab.unittest.TestCase
+
+    properties
+        tooltestingPrjRootPath
+        artifactsPath
+        demoTTPrj
+        embToolsSourcePath
+        tempFolderPath
+        testScriptPath
+        examplePrjPath
+        demoPrjName
+        demoPrjPath
+        testPrj
+    end
+    
+    properties(Constant)
+        tooltestingPrj = 'DemoToolTest.prj';
+        DemoToolPrj = 'DemoTools';
+    end
+
+% function tests = fancyTest
+% tests = functiontests(localfunctions);
+% end
+methods(TestMethodSetup)
+    function pullDependencies(testCase)
+        disp(pwd)
+        % Get details about current testing project
+        testCase.testScriptPath = fileparts(which(mfilename));
+        addpath(testCase.testScriptPath);
+                curPrj = simulinkproject;
+                testCase.tooltestingPrjRootPath = curPrj.RootFolder;
+                testCase.demoTTPrj = fullfile(testCase.tooltestingPrjRootPath, testCase.tooltestingPrj);
+       
+        testCase.artifactsPath = fullfile(testCase.tooltestingPrjRootPath, 'Tests', 'Artifacts');
+        testCase.demoPrjName = 'DemoTools.prj';
+        testCase.demoPrjPath = fullfile(testCase.artifactsPath,'DemoTools');
+        testCase.testPrj = simulinkproject(fullfile(testCase.demoPrjPath,testCase.demoPrjName));
+%         testCase.testScriptPath = fileparts(which(mfilename));
+%         addpath(testCase.testScriptPath);
+disp(pwd)
+    end
+end
+    
+methods(Test)
+function testFanciness(testCase)
+s = getFancy;
+testCase.assertEqual(s,'pretty fancy');
+end
+
+function testMatlabVer1(testCase)
+testCase.assumeTrue( verLessThan('matlab','9.10'),'Test version 20b');
+releaseInfo = matlabRelease;
+testCase.assertEqual(releaseInfo.Release,"R2020b");
+end
+
+function testMatlabVer2(testCase)
+testCase.assumeTrue( ~verLessThan('matlab','9.13'),'Test Case supported for version R2022b onwards');
+releaseInfo = matlabRelease;
+testCase.assertEqual(releaseInfo.Release,"R2022b");
+end
+end
+
+methods(TestMethodTeardown)
+    function deleteCopies(testCase)
+        bdclose all;
+        
+        try
+            prj = simulinkproject;
+            prj.close;
+        catch
+        end
+        simulinkproject(testCase.demoTTPrj)
+    end
+
+end
+end
